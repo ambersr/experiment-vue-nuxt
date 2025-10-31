3813832f6b4fd7480d9bf15e9dedbce853a35482
ambersr <amber.schalker@hva.nl>
2025-10-31 21:29:56 +0100

    feat: component document card #4

---
+++ app/components/documents/DocumentCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  document: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <article>
+    <h2 class="truncate two">{{ document.title }}</h2>
+    <p class="truncate two">{{ document.description }}</p>
+
+    <NuxtLink class="button-outline-blue" :to="`/${document.slug}`">
+      Meer informatie
+    </NuxtLink>
+  </article>
+</template>
