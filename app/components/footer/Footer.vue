430f994176a41b15084d9dfa550a13a31cfa3fe5
ambersr <amber.schalker@hva.nl>
2025-10-31 21:29:27 +0100

    feat: footer en header component #4

---
+++ app/components/footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <p>Dit is de footer</p>
+    <NuxtLink to="/">Menu item</NuxtLink>
+    <NuxtLink to="/">Menu item</NuxtLink>
+    <NuxtLink to="/">Menu item</NuxtLink>
+  </footer>
+</template>
+
---
+++ app/components/header/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+      <NuxtLink to="/" class="first">Menu item</NuxtLink>
+
+      <div class="links">
+        <NuxtLink to="/">Menu item</NuxtLink>
+        <NuxtLink to="/">Menu item</NuxtLink>
+      </div>
+    </nav>
+</template>
